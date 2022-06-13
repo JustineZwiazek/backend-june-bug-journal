adc447abfa5265cb44a21ca175e3df9be39243ad
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-06-13 21:59:44 +0200

    Added schemas

--- server.js
+++ server.js
@@ -1,9 +1,132 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import { readFile } from "fs/promises";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/junebugjournal";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// --------------- SCHEMAS ----------------- //
+// ----- User Schema ----- //
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+  location: {
+    type: String,
+  },
+  name: {
+    type: String,
+    trim: true,
+    minlength: 2,
+  },
+  created: {
+    type: Number,
+    default: () => Date.now(),
+    timestamps: true,
+  },
+});
+
+// ----- Plant Schema ----- //
+const PlantSchema = new mongoose.Schema({
+  body: {
+    id: "Number",
+    name: "String",
+    class: "String",
+    type: "String",
+    years: "String",
+    position: "String",
+    height: "Number",
+    sowing_type: "String",
+    sowing_start: "String",
+    sowing_end: "String",
+    harvest_start: "String",
+    harvest_end: "String",
+    days_germination: "Number",
+    days_harvest: "String",
+    description: "String",
+    cutivation_info: "String",
+  },
+  time: {
+    type: Date,
+  },
+  createdAt: {
+    type: Number,
+    default: () => Date.now(),
+  },
+  // I am not sure this is the way to go:
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// ----- Task Schema ----- //
+const TaskSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    minlength: 3,
+    maxlength: 150,
+    trim: true,
+  },
+  time: {
+    type: Date,
+  },
+  createdAt: {
+    type: Number,
+    default: () => Date.now(),
+  },
+  // I am not sure this is the way to go:
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// ----- Note Schema ----- //
+const NoteSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    minlength: 8,
+    maxlength: 600,
+    trim: true,
+  },
+  time: {
+    type: Date,
+  },
+  createdAt: {
+    type: Number,
+    default: () => Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+// ----- Journal Schema ----- //
+const JournalSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
 const port = process.env.PORT || 8080;
 const app = express();
 
